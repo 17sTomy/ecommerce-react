8586240cd3934dc1f457cc0c4d5de94304171f89
17sTomy <tomasignacioalv@hotmail.com>
2024-05-13 13:13:04 -0300

    Add local storage functionality to persist cart data

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import { createContext, useState } from "react";
 
 const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const storedCart = localStorage.getItem("cart");
+    storedCart ? setCart(JSON.parse(storedCart)) : null;
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   const addToCart = (item) => {
     const itemInCartIndex = cart.findIndex(product => product.id === item.id);
