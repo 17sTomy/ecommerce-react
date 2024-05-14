21f2339519e1af312478272464091415e740332e
17sTomy <tomasignacioalv@hotmail.com>
2024-05-14 20:16:56 -0300

    Add delete button to remove items from cart and to clear cart in checkout page

--- src/components/CheckoutProducts.jsx
+++ src/components/CheckoutProducts.jsx
@@ -2,15 +2,17 @@ import { useContext } from "react";
 import Card from 'react-bootstrap/Card';
 import CartContext from "../context/CartContext";
 import CartButton from "../components/CartButton";
+import { MdDelete } from "react-icons/md";
+import "../styles/Checkout.css";
 
 export default function CheckoutProducts() {
-  const { cart } = useContext(CartContext);
+  const { cart, removeAllFromCart } = useContext(CartContext);
 
   return (
     <>
       {cart.map((item) => (
-        <>
-          <div key={item.id} className="d-flex align-items-center">
+        <div key={item.id} className="checkout-product">
+          <div className="d-flex align-items-center">
             <div style={{ marginRight: '10px' }}>
               <Card.Img variant="top" src={item.image} style={{ objectFit: 'contain', width: '100px', height: '100px' }} />
             </div>
@@ -20,8 +22,9 @@ export default function CheckoutProducts() {
               <CartButton productData={item} />
             </div>
           </div>
+          <MdDelete className="delete-icon" onClick={() => removeAllFromCart(item)} />
           <hr />
-        </>
+        </div>
       ))}
     </>
   );
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,16 +1,25 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import CartContext from "../context/CartContext";
+import Button from 'react-bootstrap/Button';
+import { MdDelete } from "react-icons/md";
+import { FaShoppingBag } from "react-icons/fa";
 import CheckoutProducts from "../components/CheckoutProducts";
 import CheckoutForm from "../components/CheckoutForm";
 
 export default function Checkout() {
-  const { calculateTotal } = useContext(CartContext);
+  const { calculateTotal, clearCart } = useContext(CartContext);
 
   return (
     <div className="container">
       <div className="d-flex flex-column flex-md-row" style={{ width: "100%" }}>
         <div className="d-flex flex-column" style={{ width: "100%" }}>
-          <h2 className="mt-3 mb-4 mx-2">Cart - Total: ${calculateTotal().toFixed(2)}</h2>
+          <h2 className="mt-3 mb-4">Total: ${calculateTotal().toFixed(2)}</h2>
+          {calculateTotal() > 0 ? (
+            <Button onClick={clearCart}  className='mb-4 custom-button-clear-cart' type="submit">Clear Cart<MdDelete className="mb-1 mx-2" /></Button>
+          ) : (
+            <Link to={"/"}><Button className='mb-4 custom-button' type="submit">Shop Now<FaShoppingBag className="mb-1 mx-2" /></Button></Link>
+          )}
           <CheckoutProducts />
         </div>
         <div className="d-flex flex-column mt-md-5 mx-md-5" style={{ width: "100%" }}>
