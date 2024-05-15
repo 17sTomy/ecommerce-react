8d62dd3a9883cb0f0563a343ccaaf24496538d55
17sTomy <tomasignacioalv@hotmail.com>
2024-05-14 23:15:11 -0300

    Fix import statement in CartWidget component

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FaCartShopping } from "react-icons/fa6";
 import CartContext from '../context/CartContext';
-import '../styles/CartWidget.css'
+import '../styles/CartWidget.css';
 
 export default function CartWidget() {
   const { itemsInCart } = useContext(CartContext);
